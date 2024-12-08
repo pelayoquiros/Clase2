086129032afeda484b66cd477018f7a2e0e9bd64
pelayoquiros <pelayoquiros@airdepeyoquiros.home>
2024-12-08 16:38:46 +0100

    mi primer aplicacion streamlit

---
+++ assets/ev_clase2/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+
+def main():
+    st.title("Mi primera app en Streamlit")
+    st.write("Esta es una aplicación de prueba.")
+
+if __name__ == "__main__":
+    main()
+
